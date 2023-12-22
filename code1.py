05a1b5a6d16531e11a7600266045f137d2db0f3c
Faiyaz Rahman <faiyaaaz.rahman@gmail.com>
2023-12-22 07:04:54 +0600

    first commit

--- code1.py
+++ code1.py
@@ -1,10 +1,12 @@
+#checking name if its in contact book (implemented in choice 1)
 def check_name(name):
     with open("contacts.txt", "r") as f:
         for line in f:
             existing_name, _ = line.strip().split(",")
-            if existing_name == name:
+            if existing_name.lower() == name.lower():
                 return True
     return False 
+#checking phone if its in contact book (implemented in choice 2)
 def check_phone(phone):
     with open("contacts.txt", "r") as f:
         for line in f:
@@ -12,28 +14,52 @@ def check_phone(phone):
             if existing_phone == phone:
                 return True
     return False
-
-choice = int(input("1. Add a new contact\n2. View all contacts\n3. Search for a contact, and\n4. Exit the program\n"))
-
-if choice == 1:
-    name = input("Enter name: ")
-    phone = input("Enter number: ")
-
-    if len(phone) != 5:
-        print("Enter a valid phone number.")
-    else:
-        if check_name(name) == True:
-            print("Name already used. Use a different name.")
-        elif check_phone(phone) == True:
-            print("Phone number already in contact book.")
-        else:
-            with open("contacts.txt", "a") as f:
-                f.write(f"\n{name},{phone}")
-            print("Contact saved.")
-elif choice == 2:
+#searching contact (implemented in choice 3)
+def search_contact(search_name):
     with open("contacts.txt", "r") as f:
-        count = 1
         for line in f:
-            name, phone = line.strip().split(",")
-            print(f"{count}.Contact name: {name}\nPhone number: {phone}\n")
-            count += 1
+            existing_name, existing_phone = line.strip().split(",")
+            if search_name.lower() == existing_name.lower():
+                return existing_phone
+    return False
+
+choice = 0
+while choice != 4:
+
+    choice = int(input("1. Add a new contact\n2. View all contacts\n3. Search for a contact, and\n4. Exit the program\n"))
+
+#choice 1
+    if choice == 1:
+        name = input("Enter name: ")
+        phone = input("Enter number: ")
+
+        if len(phone) != 5:  #let, valid phone number contains 5 digits 
+            print("Enter a valid phone number.") #checking if the number is valid
+        
+        else: 
+            if check_name(name) == True: 
+                print("Name already used. Use a different name.") #checking name if its in contact book
+            
+            elif check_phone(phone) == True:
+                print("Phone number already in contact book.") #checking phone if its in contact book
+            
+            else:
+                with open("contacts.txt", "a") as f:
+                    f.write(f"{name.title()},{phone}\n") #saving contact in txt file
+                print("Contact saved.")
+#choice 2   
+    elif choice == 2:
+        with open("contacts.txt", "r") as f:
+            count = 1
+            for line in f:
+                name, phone = line.strip().split(",")
+                print(f"{count}.Contact name: {name}\nPhone number: {phone}\n")
+                count += 1
+#choice 3   
+    elif choice == 3:
+        search_name = input("Enter contact name: ")
+        if search_contact(search_name) == False:
+            print("Contact not found. Try again.")
+        else:
+            print(f"Phone number: {search_contact(search_name)}")
+
